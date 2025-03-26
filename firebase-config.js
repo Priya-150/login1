6ab840d99a76a587630d0ea3a9559eaa797b5024
Priya-150 <159451684+Priya-150@users.noreply.github.com>
2025-03-25 23:55:30 -0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+
+  apiKey: "AIzaSyDVTV5-48Y0R0bMXEP719t38qnA6bdk4AY",
+  authDomain: "login-page-e0ff8.firebaseapp.com",
+  projectId: "login-page-e0ff8",
+  storageBucket: "login-page-e0ff8.firebasestorage.app",
+  messagingSenderId: "212622563742",
+  appId: "1:212622563742:web:afe0a9f6783e8a07465e8f",
+  measurementId: "G-6JLNH8VLVG"
+};
+
+
+firebase.initializeApp(firebaseConfig);
